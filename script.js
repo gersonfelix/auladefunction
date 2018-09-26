94cff0ac3ede23f46f6566c9bca80affae4595fe
gersonfelix <gersonfelixjr@gmail.com>
2018-09-26 12:08:26 -0300

    Aula do Renan sobre Function no JavaScript

---
+++ script.js
@@ -0,0 +1,15 @@
+function nome(argumento1, argumento2, argumento3){
+    execução de código
+    execução de código
+    
+return retorno;
+}
+
+function quadrado(x){
+    let resultado = x**2;
+    return resultado; 
+}
+
+function conta(x,y){
+    return[(2*x)+(3*y),(2*x)-(3*y)];
+}
